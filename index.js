0db62aba0cb7758c395bb4bf02a846f0b76cf910
Tom Lorenz <tom@lorenz-unna.de>
2019-10-15 15:42:00 +0000

    #34

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function RademacherHomePilot(log, config, api) {
                             }
                         }
                         // dimmer
-                        else if(["35140462"].includes(data.deviceNumber))
+                        else if(["35140462","35000462","35001262"].includes(data.deviceNumber))
                         {
                             if (accessory === undefined) {
                                 self.addDimmerAccessory(data);
