97b3417c58161fbf30c0e24aaba6e50b9e8f59fd
Tom Lorenz <tom@lorenz-unna.de>
2020-03-24 13:05:21 +0100

    #44 added 23602075

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function RademacherHomePilot(log, config, api) {
                         var accessory = self.accessories[uuid];
                         
                         // blinds
-                        if(["27601565","35000864","14234511","35000662","36500172","36500572_A","16234511_A","16234511_S","45059071","31500162"].includes(data.deviceNumber))
+                        if(["27601565","35000864","14234511","35000662","36500172","36500572_A","16234511_A","16234511_S","45059071","31500162","23602075"].includes(data.deviceNumber))
                         {
                             if (accessory === undefined) {
                                 self.addBlindsAccessory(data);
